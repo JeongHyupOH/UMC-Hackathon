9b30556010439bbc596e10c465807031ad627afc
최원영 <wy129@naver.com>
2024-06-28 16:52:13 +0900

    init

---
+++ src/main/java/com/circlezero/hackathon_cake/HackathonCakeApplication.java
@@ -0,0 +1,13 @@
+package com.circlezero.hackathon_cake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonCakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/circlezero/hackathon_cake/HackathonCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circlezero.hackathon_cake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
